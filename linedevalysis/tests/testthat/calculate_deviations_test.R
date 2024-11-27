574e037b6163bb8b626d5ab6bee4be7c1cc01126
George Newman <137775831+GeorgeNewman27@users.noreply.github.com>
2024-11-27 19:03:07 +0000

    Delete linedevalysis/tests directory cause it only contained garbage tests that didn't work

--- linedevalysis/tests/testthat/calculate_deviations_test.R
+++
@@ -1,6 +0,0 @@
-describe("calculate_deviations()", {
-  it("rejects inputs that aren't data frames", {
-    expect_error(calculate_deviations("test.gpx"))
-    expect_error(calculate_deviations(c(1,2)))
-  })
-})
